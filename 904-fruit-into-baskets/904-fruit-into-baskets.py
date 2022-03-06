cda5f97f74b17dda4417a2550f3f81abc44f2e46
Alejandro Barraza <71998273+alejandroBarraza@users.noreply.github.com>
2022-03-06 02:25:12 -0300

    Time: 1619 ms (11.21%), Space: 20.6 MB (12.75%) - LeetHub

---
+++ 904-fruit-into-baskets/904-fruit-into-baskets.py
@@ -0,0 +1,24 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        window_start, max_len = 0, 0
+        visited_fruits = {}
+        
+        for window_end in range(len(fruits)):
+            fruit = fruits[window_end]
+            if not fruit in visited_fruits:
+                visited_fruits[fruit] = 0
+            visited_fruits[fruit] +=1
+                
+            while len(visited_fruits) > 2:
+                fruit_delete = fruits[window_start]
+                visited_fruits[fruit_delete] -=1
+                if visited_fruits[fruit_delete] == 0:
+                    del visited_fruits[fruit_delete]
+                window_start +=1            
+            
+            max_len = max(max_len, window_end - window_start + 1  )
+            
+        return max_len
+            
+            
+        
